83163eac50f5b86a7d974ff384db872e2b5c8853
Aaron Chan <aaronclh@outlook.com>
2025-04-09 12:26:54 +1200

    component 3 version 2

--- Components/component3_menuandorder.py
+++ Components/component3_menuandorder.py
@@ -9,7 +9,7 @@ def weapon_list():
     pd.set_option('display.float_format', '{:.2f}'.format)
     
     # Add category numbers and names to dictionary
-    menu_dict["categoryID"] = [1, 2, 3, 4, 6]
+    menu_dict["categoryID"] = [1, 2, 3, 4, 5]
     menu_dict["categoryName"] = ["Medieval European", "Renaissance and Duelling", "Asian and Eastern",
                                  "Fantasy and Decorative", "Practical and Training"]
     
@@ -40,21 +40,27 @@ def weapon_list():
     }
     
     # Print all dictionaries
+    # Print Menu Categories
     print("Menu Categories:")
     print(pd.DataFrame(menu_dict, index=range(1, len(menu_dict["categoryID"]) + 1)))
     
+    # Print Europe Category
     print("\nMedieval European Swords")
     print(pd.DataFrame(menu_Europe, index=range(1, len(menu_Europe["swordType"]) + 1)))
     
+    # Print Duelling Category
     print("\nRenaissance and Duelling Swords:")
     print(pd.DataFrame(menu_Duelling, index=range(1, len(menu_Duelling["swordType"]) + 1)))
     
+    # Print Asia Category
     print("\nAsian and Eastern Swords")
     print(pd.DataFrame(menu_Asia, index=range(1, len(menu_Asia["swordType"]) + 1)))
     
+    # Print Decor Category
     print("\nFantasy and Decorative Swords")
     print(pd.DataFrame(menu_Decor, index=range(1, len(menu_Decor["swordType"]) + 1)))
     
+    # Print Practical Category
     print("\nPractical and Training Swords")
     print(pd.DataFrame(menu_Practical, index=range(1, len(menu_Practical["swordType"]) + 1)))
     
